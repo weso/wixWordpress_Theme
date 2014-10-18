e0daf13bdabfd88b89608ec2b664091e5a9b6c59
castrofernandez <castrofernandez@gmail.com>
2014-10-18 13:29:42 +0200

    Store tweets

--- inc/twitter/Twitter.php
+++ inc/twitter/Twitter.php
@@ -4,66 +4,105 @@
 	class Twitter {
 		function Twitter() {
 			$settings = json_decode(file_get_contents(__DIR__."/settings.json"), true);
-			
+
 			if(!defined('DEFAULT_ACCOUNT')) {
 				define('DEFAULT_ACCOUNT', $settings["twitterAccount"]);
 			}
-			
+
 			if(!defined('DEFAULT_HASHTAG')) {
 				define('DEFAULT_HASHTAG', $settings["twitterHashtag"]);
 			}
-			
+
 			if(!defined('TWEET_LIMIT')) {
 				define('TWEET_LIMIT', $settings["tweetsLimit"]);
 			}
-			
+
 			if(!defined('CONSUMER_KEY')) {
 				define('CONSUMER_KEY', $settings["consumerKey"]);
 			}
-			
+
 			if(!defined('CONSUMER_SECRET')) {
 				define('CONSUMER_SECRET', $settings["consumerSecret"]);
 			}
-			
+
 			if(!defined('ACCESS_TOKEN')) {
 				define('ACCESS_TOKEN', $settings["accessToken"]);
 			}
-			
+
 			if(!defined('ACCESS_TOKEN_SECRET')) {
 				define('ACCESS_TOKEN_SECRET', $settings["accessTokenSecret"]);
 			}
 		}
-	
+
 		function loadDefaultAccountTweets() {
-			return $this->loadTweets(DEFAULT_ACCOUNT, DEFAULT_HASHTAG);
+			$tweets = $this->loadTweetsByHashtag(DEFAULT_ACCOUNT, DEFAULT_HASHTAG);
+
+			if (count($tweets) < TWEET_LIMIT && file_exists(__DIR__."/tweets.json")) {
+				$old_tweets = json_decode(file_get_contents(__DIR__."/tweets.json"), true);
+
+				$number = TWEET_LIMIT - count($tweets);
+
+				for ($i = 0; $i < count($old_tweets) && $i < $number; $i++)
+					array_push($tweets, $old_tweets[$i]);
+			}
+
+			// Save updated tweets
+			$fp = fopen(__DIR__."/tweets.json", 'w');
+			fwrite($fp, json_encode($tweets));
+			fclose($fp);
+
+			return $tweets;
 		}
-	
-		function loadTweets($account, $hashtag) {
+
+		function loadTweets($account) {
 			$twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
-		
-			$tweets = $twitter->get('statuses/user_timeline', 
-									array('screen_name' => $account, 
-											'exclude_replies' => 'true', 
-											'include_rts' => 'true', 
-											'count' => TWEET_LIMIT));
-																	
+
+			$search = array('screen_name' => $account,
+											'exclude_replies' => 'true',
+											'include_rts' => 'true',
+											'count' => TWEET_LIMIT);
+
+			$tweets = $twitter->get('statuses/user_timeline', $search);
+
+			return $this->loopThroughTweets($tweets, $account);
+		}
+
+		function loadTweetsByHashtag($account, $hashtag) {
+			$twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+
+			$search = array('q' => "from:$account+#$hashtag");
+
+			$tweets = $twitter->get('search/tweets', $search);
+
+			if (!empty($tweets) && property_exists($tweets, 'statuses'))
+				$tweets = $tweets->statuses;
+			else
+				$tweets = array();
+
+			return $this->loopThroughTweets($tweets, $account);
+		}
+
+		function loopThroughTweets($tweets, $account) {
 			if (!empty($tweets)) {
 				$processed_tweets = array();
-		
+
 				foreach($tweets as $tweet) {
 					// Si es un retweet
 					if (property_exists($tweet, 'retweeted_status'))
 						$tweet = $tweet->retweeted_status;
-				
+
 					$text = $this->processTweetText($tweet);
-				
+
 					$date = date("j M Y", strtotime($tweet->created_at));
-					
+
 					$username = $tweet->user->name;
 					$screen_name =  $tweet->user->screen_name;
 					$account_image = $tweet->user->profile_image_url;
 					$language = $tweet->lang;
-				
+
+					if ($screen_name != $account)
+						continue;
+
 					$tweet = array(
 						'content' => $text,
 						'account' => array(
@@ -72,50 +111,50 @@
 							'img' => $account_image
 						)
 					);
-				
+
 					array_push($processed_tweets, $tweet);
 				}
-			
+
 				return $processed_tweets;
 			}
-										
-			return null;
+
+			return array();
 		}
-	
+
 		function processTweetText($tweet) {
 			$replace_index = array();
-		
+
 			$text = $tweet->text;
-		
+
 			if (property_exists($tweet, 'entities') ) {
 				foreach ($tweet->entities as $area => $items) {
-				
+
 					foreach ($items as $item) {
 						$entity = $this->processEntity($area, $item);
 						$href = $entity['href'];
 						$string = $entity['string'];
-						$prefix = $entity['prefijo'];
-					
-						if (!(strpos($href, 'http://') === 0)) 
+						$prefix = $entity['prefix'];
+
+						if (!(strpos($href, 'http://') === 0))
 							$href = "http://".$href;
-						
+
 						$index = substr($text, $item->indices[0], $item->indices[1]-$item->indices[0]);
-						$replace = "<a class=\"enlace\" href=\"$href\">{$prefix}{$string}</a>";
+						$replace = "<a class=\"link\" href=\"$href\">{$prefix}{$string}</a>";
 						$replace_index[$index] = $replace;
 					}
 				}
-			
-				foreach ($replace_index as $replace => $with) 
+
+				foreach ($replace_index as $replace => $with)
 					$text = str_replace($replace, $with, $text);
 			}
-		
+
 			return $text;
 		}
-	
+
 		function processEntity($area, $item) {
 			$href = '';
 			$string = '';
-		
+
 			switch ( $area ) {
 				case 'hashtags':
 					$prefix = '#';
@@ -129,18 +168,18 @@
 					$string = $item->screen_name;
 					$href = $url.$string;
 					break;
-				case 'media': 
+				case 'media':
 				case 'urls':
 					$prefix = '';
 					$url = '';
 					$string = $item->display_url;
 					$href = $item->expanded_url;
 					break;
-				default: 
+				default:
 					break;
 			}
-		
-			return array('href' => $href, 'string' => $string, 'prefijo' => $prefix);
+
+			return array('href' => $href, 'string' => $string, 'prefix' => $prefix);
 		}
 	}
 ?>
