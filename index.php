1bcbd2026bba0068108212bb68a0606ea50afc3f
root <root@gozon.weso>
2014-11-04 19:22:15 +0100

    Restore index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-	echo '0';
  	require_once(get_stylesheet_directory().'/renderization/controller.php');
-	echo '1';
         $controller = Controller::getInstance();
 	
-	echo '2';
         $renderer = $controller->renderer;
 
 	get_header();
