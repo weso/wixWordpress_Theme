f503db0e88c04b82e8c03387722e6b3565cd05f6
castrofernandez <castrofernandez@gmail.com>
2014-12-06 12:42:30 +0100

    Report ranking table

--- scripts/report.js
+++ scripts/report.js
@@ -1,5 +1,5 @@
 (function() {
-  var anchor, container, nav, position, visualisation, visualisations, wrapper, _i, _len;
+  var anchor, container, nav, position, rankingTable, viewMore, visualisation, visualisations, wrapper, _i, _len, _ref, _ref1;
 
   visualisations = document.querySelectorAll(".hidden-visualisations div.visualisation");
 
@@ -26,6 +26,38 @@
       continue;
     }
     nav.parentNode.insertBefore(visualisation, nav.nextSibling);
+  }
+
+  rankingTable = document.querySelector("section.ranking-table");
+
+  viewMore = rankingTable.querySelector("a.ranking-view-more");
+
+  if (rankingTable && viewMore) {
+    viewMore.onclick = function(event) {
+      var opened;
+      opened = rankingTable.opened ? rankingTable.opened : false;
+      opened = !opened;
+      rankingTable.opened = opened;
+      rankingTable.className = opened ? "report-ranking opened" : "report-ranking";
+      return this.innerHTML = opened ? "<span>&#171;</span> VIEW LESS" : "VIEW MORE <span>&#187;</span>";
+    };
+  }
+
+  wrapper = document.querySelector(".ranking-wrapper");
+
+  if (wrapper) {
+    wrapper.appendChild(rankingTable);
+    if (rankingTable != null) {
+      if ((_ref = rankingTable.style) != null) {
+        _ref.display = "table";
+      }
+    }
+  } else {
+    if (rankingTable != null) {
+      if ((_ref1 = rankingTable.style) != null) {
+        _ref1.display = "none";
+      }
+    }
   }
 
 }).call(this);
